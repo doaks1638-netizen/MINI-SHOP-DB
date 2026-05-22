fe717d18946cbf73a4b979e08938f308fcd47dd5
Alex <doaks1638@gmail.com>
2026-05-23 01:03:29 +0300

    Удалены лишиние файлы и пару ненужных функций

--- app/config.py
+++ app/config.py
@@ -13,19 +13,11 @@ class Db_config:
         self.dbname = os.getenv('DBNAME')
         self.user = os.getenv('USER')
         self.password = os.getenv('PASSWORD')
-        self._log_show = False
-
+        
     def get_for_connect(self):
-        if not self._log_show:
-            print("--- ЛОГ СИСТЕМНОЙ КОНФИГУРАЦИИ ---")
-            print(f"БД Хост:      {self.host}")
-            print(f"БД Порт:      {self.port}")
-            print(f"БД Имя:       {self.dbname}")
-            print(f"Пользователь: {self.user}")
-            print(f"Пароль: {self.password}")
-            self._log_show = True
         return {'host':self.host, 'port':self.port, 
                 'dbname':self.dbname,'user':self.user,
                 'password':self.password}
+
 
 DB = Db_config()
--- app/db.py
+++ app/db.py
@@ -2,6 +2,6 @@
 import psycopg
 from psycopg.rows import namedtuple_row
 from config import DB
-from db import get_connect
+
 def get_connect():
     return psycopg.connect(**DB.get_for_connect(), row_factory=namedtuple_row)
