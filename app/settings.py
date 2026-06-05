4ec648589419d3bcc54d4f2261f1f4f9332f9829
Alex <doaks1638@gmail.com>
2026-06-05 12:18:57 +0300

    full rewrite of project structure; add .env.example

---
+++ app/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+parent_path = Path(__file__).parent.parent  # path to .env file in project path
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: int
+    DB_PORT: int
+    DB_NAME: str
+
+    def get_db_url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(extra="ignore", env_file=parent_path / ".env")
+
+
+settings = Settings()  # make settings singlitone
