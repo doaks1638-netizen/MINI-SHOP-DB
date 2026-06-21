34530665a0afa8eeb8a347444b37f0e464917180
Alex <doaks1638@gmail.com>
2026-06-22 00:03:20 +0300

    add fastapi.BackgroudTasks for yookassa payment - that can finnally fix bug when yookassa wait ONLY 2 seconds for my responce, but my fastapi is not very fast(((.
    also add loguru for log backgorund tasks. I know that payment with BackGroundTasks is very bad idea (crash server, other bugs...), but it is good for my first project! Also I add celery + redis when I learn it!!!

--- backend/app/routes/v1/payments.py
+++ backend/app/routes/v1/payments.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, Request, HTTPException, BackgroundTasks
 import ipaddress
-from app.database import DBsession
+from app.database import SessionLocal
 from json import JSONDecodeError
-from yookassa.domain.notification import WebhookNotificationFactory
+from yookassa.domain.notification import WebhookNotification
 from sqlalchemy import select
 from app.models import Payment
 from app.models.enums import PaymentStatus
 from app.services import debit_funds
+from loguru import logger
 
 payment_router = APIRouter(
     prefix="/payments",
@@ -53,8 +54,34 @@ def _extract_client_ip(request: Request) -> str | None:
     return request.client.host if request.client else None
 
 
+async def yookassa_webhook_func(payment):
+    async with SessionLocal() as db:
+        # Сам идентификатор платежа ЮKassa (тот самый yookassa_id)
+        yookassa_id = payment.id
+
+        if not yookassa_id:
+            logger.error("Missing yookassa id")
+
+        payment_db = await db.scalar(
+            select(Payment).where(Payment.yookassa_id == yookassa_id)
+        )
+        if payment_db is None:
+            return {"status": "ignored"}
+
+        if payment.status == "succeeded":
+            if payment_db.status != PaymentStatus.succeeded:
+                payment_db.status = PaymentStatus.succeeded
+                await debit_funds(db, payment_db.user_id, payment_db.amount)
+
+        elif payment.status == "canceled":
+            if payment_db.status != PaymentStatus.canceled:
+                payment_db.status = PaymentStatus.canceled
+
+        await db.commit()
+
+
 @payment_router.post("/yookassa/webhook")
-async def yookassa_webhook(db: DBsession, request: Request):
+async def yookassa_webhook(request: Request, background_tasks: BackgroundTasks):
     client_ip = _extract_client_ip(request)
 
     if not check_ip(client_ip):
@@ -66,31 +93,8 @@ async def yookassa_webhook(db: DBsession, request: Request):
         raise HTTPException(400, detail=f"Bad payload. Exception - {exc}")
 
     try:
-        notification = WebhookNotificationFactory().create(payload)
-    except Exception as exc:
-        raise HTTPException(400, detail=f"Bad notification")
-
-    payment = notification.object
-    # Сам идентификатор платежа ЮKassa (тот самый yookassa_id)
-    yookassa_id = payment.id
-
-    if not yookassa_id:
-        raise HTTPException(status_code=400, detail="Missing yookassa id")
-
-    payment_db = await db.scalar(
-        select(Payment).where(Payment.yookassa_id == yookassa_id)
-    )
-    if payment_db is None:
-        return {"status": "ignored"}
-
-    if payment.status == "succeeded":
-        if payment_db.status != PaymentStatus.succeeded:
-            payment_db.status = PaymentStatus.succeeded
-            await debit_funds(db, payment_db.user_id, payment_db.amount)
-
-    elif payment.status == "canceled":
-        if payment_db.status != PaymentStatus.canceled:
-            payment_db.status = PaymentStatus.canceled
-
-    await db.commit()
+        notification = WebhookNotification(payload)
+    except Exception:
+        raise HTTPException(400, detail="Bad notification")
+    background_tasks.add_task(yookassa_webhook_func, notification.object)
     return {"status": "ok"}
